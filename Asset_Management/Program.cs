1d22285731e1f3f30de6612add92dd75018f8a2d
Pratik <pratik.balkunde@nitorinfotech.com>
2023-04-07 14:35:23 +0530

    Explanation in Program.cs

--- Asset_Management/Program.cs
+++ Asset_Management/Program.cs
@@ -1,3 +1,5 @@
+//1 The first few lines of the code import various namespaces from the .NET framework
+//and external libraries that will be used in the application.
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -8,21 +10,33 @@ using Asset_Management.CustomFilters;
 using Asset_Management.Services.Interface;
 using Asset_Management.Services.Implementation;
 
+//2 The WebApplication.CreateBuilder(args) method is called to
+//create a new instance of the WebApplicationBuilder class,
+//which is used to configure and build the application.
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 3 The builder.Services.AddAuthentication method is called to configure authentication in the application.
+// Specifically, it sets up JWT bearer authentication using the JwtBearerDefaults.
+// AuthenticationScheme, and specifies that authentication will be handled by Azure AD
+// using the configuration settings in the "AzureAd" section of the application's configuration file.
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
 
-//Register the CompanyContext in DI Container
+//4 The builder.Services.AddDbContext method is called to register the asset_managementContext class
+//in the dependency injection (DI) container.
+//This class is a database context class that is used to interact with a SQL Server database.
 builder.Services.AddDbContext<asset_managementContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("AppConnStr"));
 });
 
 
-//Register Service Classes in DI
+//5 The builder.Services.AddScoped method is called several times to register various service classes in the DI container.
+//These service classes implement the IService interface,
+//which provides basic CRUD operations for working with data in the application.
+//There are also two service classes that implement custom interfaces (IAssetDetailService and IAssetTransactionService)
+//that provide additional functionality for working with asset detail and asset transaction data.
 builder.Services.AddScoped<IService<User, int>, UserService>();
 builder.Services.AddScoped<IService<AssetDetail, int>, AssetDetailService>();
 builder.Services.AddScoped<IService<AssetTransaction, int>, AssetTransactionService>();
@@ -32,31 +46,47 @@ builder.Services.AddScoped<IAssetTransactionService<AssetTransaction,string>, As
 builder.Services.AddScoped<IAuthService<User,int>,AuthService>();
 builder.Services.AddScoped<IVendorService<Vendor, int>, VendorService>();
 
+//6 The builder.Services.AddControllers method is called to register
+//the ASP.NET Core MVC controllers in the DI container.
+//The options parameter is used to configure various options for the controller feature,
+//such as filters and formatters.
 builder.Services.AddControllers(options => {
     options.Filters.Add(typeof(CustomLogFilterAttribute));
     //options.Filters.Add(typeof(CustomExceptionFilterAttribute));
 });
+
+//7 The builder.Services.AddEndpointsApiExplorer method is called to enable the
+//generation of Swagger/OpenAPI documentation for the application's API endpoints.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+//8 The builder.Services.AddSwaggerGen method is called to configure the Swagger/OpenAPI generator.
 builder.Services.AddSwaggerGen();
 
+//9 The builder.Build() method is called to build the application and
+//create an instance of the WebApplication class.
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 10 The app.Environment.IsDevelopment() method is called to determine if the application is running in development mode.
+// If so, the Swagger/OpenAPI documentation is enabled using the app.UseSwagger() and app.UseSwaggerUI() methods.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+//11 The app.UseHttpsRedirection() method is called to enable HTTP to HTTPS redirection.
 app.UseHttpsRedirection();
 
 //Use Custom Exception
  app.UseAppException();
 
+//12 The app.UseAuthentication() and app.UseAuthorization() methods are called
+//to enable authentication and authorization in the application.
 app.UseAuthentication();
 app.UseAuthorization();
 
+//13 The app.MapControllers() method is called to map the application's API controllers to their respective routes.
 app.MapControllers();
 
+//14 Finally, the app.Run() method is called to start the application and begin listening for incoming requests.
 app.Run();
